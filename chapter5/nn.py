e625a1f3e3f5bc302c3d335c4bca95f61b0bd4d7
sklee <hadesleeq@gmail.com>
2018-08-22 10:11:09 +0900

    한글번역

--- chapter5/nn.py
+++ chapter5/nn.py
@@ -10,36 +10,36 @@ class NeuralNetwork:
     """ 3 layer neural network """
 
     def __init__(self, input_size, hidden_size, output_size, alpha=0.1):
-        # 中間層と出力層の重みの初期化
+        # 중간층과 충력층의 가중치 초기화
         self.hw = np.random.random_sample((hidden_size, input_size + 1))
         self.ow = np.random.random_sample((output_size, hidden_size + 1))
-        # 学習率をセット
+        # 학습률을 설정
         self.alpha = alpha
 
     def learn(self, data, epoch):
-        # 重みを学習する
-        # ループ回数は、 epoch で設定する
+        # 가중치를 학습한다.
+        # 루프 횟수는 epoch으로 설정
         for epo in range(epoch):
             for t, x in data:
                 self.update_weight(t, x)
 
     def predict(self, x):
-        # ある入力ベクトル x の出力結果を得る
+        # 어떤 입력 벡터 x의 출력 결과를 얻는다.
         z, y = self.forward(x)
         return y
 
     def f(self, vec):
-        # 活性化関数 f を定義
-        # この実装は、ロジスティックシグモイド関数
+        # 활성화 함수 f를 정의
+        # 이 구현은 로지스틱 시그모이드 함수
         return np.vectorize(lambda x: 1.0 / (1.0 + math.exp(-x)))(vec)
 
     def f_dash(self, vec):
-        # 活性化関数 f の微分を定義
-        # この実装は、ロジスティックシグモイド関数
+        # 활성화 함수 f의 미분을 정의
+        # 이 구현은 로지스틱 시그모이드 함수
         return np.vectorize(lambda x: (1.0 - x) * x)(vec)
 
     def forward(self, x):
-        # ネットワークを順方向に伝搬して結果を得る
+        # 네트워크를 순방향으로 전파해서 결과를 얻는다.
 
         hidden_x = np.r_[np.array([1.0]), x]
         hidden_u = self.hw.dot(hidden_x)
@@ -52,11 +52,11 @@ class NeuralNetwork:
         return (hidden_z, output_z)
 
     def update_weight(self, t, x):
-        # 重みを更新する
+        # 가중치를 갱신한다.
 
         hidden_z, output_z = self.forward(x)
 
-        # 修正値を逆伝搬させて、重みを修正する
+        # 수정 값을 역전파시켜서 가중치를 수정한다.
         output_x = np.r_[np.array([1.0]), hidden_z]
         output_delta = (output_z - t) * self.f_dash(output_z)
         self.ow -= self.alpha * output_delta.reshape((-1, 1)) * output_x
